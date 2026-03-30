c977640ea4dbb6015b5dc5f3f86a0245c4d57bdc
Mitchman215 <85707581+Mitchman215@users.noreply.github.com>
2026-03-30 15:08:29 -0500

    flake lock

--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
 {
   description = "Bootstrap GPG + SSH with YubiKey on a new machine";
 
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      forSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+      forSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
     in
     {
-      apps = forSystems (system:
+      apps = forSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           bootstrap = pkgs.writeShellApplication {
             name = "yubikey-bootstrap";
-            runtimeInputs = with pkgs; [ gnupg pinentry-curses curl openssh git ];
+            runtimeInputs = with pkgs; [
+              gnupg
+              pinentry-curses
+              curl
+              openssh
+              git
+            ];
             text = builtins.readFile ./bootstrap.sh;
           };
         in
